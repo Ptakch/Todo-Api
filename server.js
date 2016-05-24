d69ee0a9f75653aabd1cb23eb17e648e7f1fe743
Phill Westend <phill@westendbackpackers.com>
2016-05-24 13:07:23 +1000

    '/Post_complete'

--- server.js
+++ server.js
@@ -1,24 +1,12 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var PORT = process.env.PORT || 3000;
 
-var todos = [{
-	id: 1,
-	description: 'Meet mom for lunch',
-	completed: false
-	
-},
-{
-	id: 2,
-	description: 'go to market',
-	completed: false
-},
-{
-	id: 3,
-	description: 'throw ducks at people',
-	completed: false
-}];
+var todos = [];
+var todoNextId = 1;
 
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
 	res.send('API ROOT');
@@ -46,6 +34,13 @@ app.get('/todos/:id', function(req, res){
 
 });
 
+app.post('/todos', function(req, res){
+	var body = req.body;
+	body.id = todoNextId++;
+	todos.push(body);
+	
+	res.json(body);
+});
 
 app.listen(PORT, function(){
 	console.log('Express listening on port ' + PORT + '!');
