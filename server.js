01ad40788343f4470167ff12fa0becf53cbaff8c
Phill Westend <phill@westendbackpackers.com>
2016-06-01 15:33:16 +1000

    'PUT_complete'

--- server.js
+++ server.js
@@ -47,8 +47,7 @@ app.post('/todos', function(req, res){
 
 app.delete('/todos/:id', function(req, res){
 	var todoId = parseInt(req.params.id);
-	var matchedTodo = _.findWhere(todos, {id: todoId})
-	
+	var matchedTodo = _.findWhere(todos, {id: todoId})	
 
 	if (!matchedTodo){
 		res.status(404).json({"error": "no todo found with that id"});		
@@ -56,6 +55,33 @@ app.delete('/todos/:id', function(req, res){
 		todos = _.without(todos, matchedTodo);
 		res.json(matchedTodo);
 	}
+});
+
+app.put('/todos/:id', function(req, res){
+	var todoId = parseInt(req.params.id);
+	var matchedTodo = _.findWhere(todos, {id: todoId})	
+	var body = _.pick(req.body, 'completed', 'description');
+	var validAttributes = {};
+
+	if (!matchedTodo){
+		return res.status(404).send();
+	}
+	
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')){
+		return res.status(400).send();
+	};
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')){
+		return res.status(400).send();
+	};
+	
+
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
 
 });
 
