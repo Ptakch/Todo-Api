0be3406cd0ff6aa8de678adfd0d155c01e8deded
Phill Westend <phill@westendbackpackers.com>
2016-06-01 10:43:30 +1000

    'refactor_'

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var PORT = process.env.PORT || 3000;
 
 var todos = [];
@@ -18,13 +20,7 @@ app.get('/todos', function(req, res){
 
 app.get('/todos/:id', function(req, res){
 	var todoId = parseInt(req.params.id);
-	var matchedTodo;
-
-	todos.forEach(function(todo){
-		if (todoId === todo.id){
-			matchedTodo = todo;
-		}
-	});
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 
 	if (matchedTodo){
 		res.json(matchedTodo);
@@ -35,7 +31,14 @@ app.get('/todos/:id', function(req, res){
 });
 
 app.post('/todos', function(req, res){
-	var body = req.body;
+	var body = _.pick(req.body, 'completed', 'description');
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
+
 	body.id = todoNextId++;
 	todos.push(body);
 	
